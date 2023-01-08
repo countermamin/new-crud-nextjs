d43aae716f2565c315776818282edb0b89e8d581
Orzubek Mamiinov <orzu.maminov9@gmail.com>
2023-01-09 01:53:08 +0600

    getData

--- components/table.js
+++ components/table.js
@@ -1,7 +1,15 @@
 import { BiEdit, BiTrashAlt } from 'react-icons/bi';
-import data from '../database/data.json';
+import { getUsers } from '../lib/helper';
+import { useQuery } from 'react-query';
 
 export default function Table() {
+
+	const {isLoading, isError, data, error} = useQuery('users', getUsers)
+
+	if (isLoading) return <div>Employee is Loading...</div>;
+	if (isError) return <div>Got Error {error}</div>
+
+
 	return (
 		<table className='min-w-full table-auto'>
 			<thead>
@@ -39,7 +47,11 @@ function Tr({ id, name, avatar, email, salary, date, status }) {
 	return (
 		<tr className='bg-gray-50 text-center'>
 			<td className='px-16 py-2 flex flex-row items-center'>
-				<img src={avatar || '#'} alt='img' width={60} height={60} />
+				<img
+					src={avatar || '#'}
+					alt='img'
+					className='h-8 w-8 rounded-full object-cover'
+				/>
 				<span className='text-center ml-2 font-semibold'>
 					{name || 'Unknown'}
 				</span>
@@ -55,7 +67,7 @@ function Tr({ id, name, avatar, email, salary, date, status }) {
 			</td>
 			<td className='px-16 py-2'>
 				<button className='cursor'>
-					<span className='bg-green-500 text-white px-5 py-1 rounded-full'>
+					<span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>
 						{status || 'Unknown'}
 					</span>
 				</button>
--- database/controller.js
+++ database/controller.js
@@ -1,5 +1,6 @@
 import Users from '../model/user';
 
+// get
 export async function getUsers(req, res) {
 	try {
 		const users = await Users.find({});
@@ -12,6 +13,22 @@ export async function getUsers(req, res) {
 	}
 }
 
+export async function getUser(req, res) {
+	try {
+		const {userId} = req.query;
+
+		if (userId) {
+			const user = await Users.findById(userId);
+			res.status(200).json(user);
+		}
+		res.status(404).json({error: 'User Not Selected...!'})
+
+	} catch (error) {
+		res.status(404).json({error: 'Cannot Get the User..!'})
+	}
+}
+
+//post
 export async function postUser(req, res) {
 	try {
 		const formData = req.body;
@@ -22,5 +39,39 @@ export async function postUser(req, res) {
 		});
 	} catch (error) {
 		return res.status(404).json({ error });
+	}
+}
+
+// put
+export async function putUser(req, res) {
+	try {
+		const {userId} = req.query;
+		const formData = req.body;
+
+		if (userId && formData) {
+			const user = await Users.findByIdAndUpdate(userId, formData);
+			res.status(200).json(user);
+		}
+		res.status(404).json({error: 'User Not Selected...!'})
+
+	} catch (error) {
+		res.status(404).json({error: 'Error While Updating the Data'})
+	}
+};
+
+// delete
+export async function deleteUser(req, res) {
+	try {
+		const {userId} = req.query;
+
+		if (userId) {
+			const user = await Users.findByIdAndDelete(userId);
+			return res.status(200).json(user);
+		}
+
+		res.status(404).json({error: 'User Not Selected'})
+
+	} catch (error) {
+		res.status(404).json({error: 'Error While Deleting the User...!'})
 	}
 }
